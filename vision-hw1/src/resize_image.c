fa83f57eeaaa89cc435e166d727719384620c2de
Vipin <vipin.sharma2024@gmail.com>
2019-12-06 14:36:01 +0530

    Added displacement factor as mentioned in lecture. nn_interpolate() completed

--- vision-hw1/src/resize_image.c
+++ vision-hw1/src/resize_image.c
@@ -14,7 +14,7 @@ float nn_interpolate(image im, float x, float y, int c)
 image nn_resize(image im, int w, int h)
 {
     // TODO Fill in (also fix that first line)
-    float w_scaling_factor, h_scaling_factor;
+    float w_scaling_factor, w_displacement_factor, h_scaling_factor, h_displacement_factor;
     int i, j, k;
 
     image resized_image = make_image(w, h, im.c);
@@ -23,16 +23,18 @@ image nn_resize(image im, int w, int h)
     	If original image is of size 4x4x3 and resized image is tobe of size 7x7x3, then:
     	7 pixels of new image = 4 pixels of original image
     	1 pixel of new image = 4/7 pixel of original image
-
+		Aprt from this, we will also have to take into account the fact the the pixel actually at (-0.5, -0.5) instead of (0, 0)
     	This logic will be used to map pixel coordinate of new image to old image, and then will be interpolated using nn_interpolate function to get exact pixel values of new image.
     */
     w_scaling_factor = (float)im.w/w;
     h_scaling_factor = (float)im.h/h;
+    w_displacement_factor = -0.5 + 0.5*w_scaling_factor;
+    h_displacement_factor = -0.5 + 0.5*h_scaling_factor;
 
     for(i=0; i<w; i++){
     	for(j=0; j<h; j++){
     		for(k=0; k<im.c; k++){
-    			set_pixel(resized_image, i, j, k, nn_interpolate(im, i*w_scaling_factor, j*h_scaling_factor, k));
+    			set_pixel(resized_image, i, j, k, nn_interpolate(im, (i*w_scaling_factor) + w_displacement_factor, (j*h_scaling_factor) + h_displacement_factor, k));
     		}
     	}
     }
